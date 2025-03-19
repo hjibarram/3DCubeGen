e5871068d48657dd51b3ea685eb4b64db2bc4efa
hjibarram <hjibarram@gmail.com>
2025-03-18 19:15:52 -0600

    s

--- CubeGen/tools/deconvolve.py
+++ CubeGen/tools/deconvolve.py
@@ -16,12 +16,15 @@ def psfG(psf_x=1.33,psf_y=1.33,nx=35,ny=35):
     # Generate a 2D PSF
     psf_generator = SPSFGaussian((psf_x, psf_y), (nx, ny))
     psf = psf_generator()
-    #psf.numpy()
+    psf.numpy()
     return psf
 
 def deconvolve_2dmap(image,psf_x=1.33,psf_y=1.33,nxpsf=35,nypsf=35,niter=10):
     psf=psfG(psf_x=psf_x,psf_y=psf_y,nx=nxpsf,ny=nypsf)
     imageT=np.copy(image)
+    imageT[np.where(np.isfinite(imageT) == False)]=0.0
+    imageT[np.where(np.isnan(imageT))]=0.0
+    imageT[np.where(imageT <= 0.01)]=0.0
     imageT=imageT/np.nanmax(imageT)
     deconvolved_RL = restoration.richardson_lucy(imageT, psf, num_iter=niter)
     fac=np.nanmean(image)/np.nanmean(deconvolved_RL)
