d15b604d5c497f15ab675d4ea44ccb2b05e2d2da
hjibarram <hjibarram@gmail.com>
2025-04-22 17:31:59 -0600

    Update tools.py

--- CubeGen/tools/tools.py
+++ CubeGen/tools/tools.py
@@ -580,9 +580,8 @@ def weighterror2(St,Wt,multiT=True,nprocf=6,verbose=True,matf=True):
                 for j in range(a1,a2):
                     c1=j*nly
                     c2=(j+1)*nly
-                    print(result[0].shape,a1,a2)
-                    outf[b1:b2,c1:c2]=result[0][a1-j]
-                    dist[b1:b2,c1:c2]=result[1][a1-j]
+                    outf[b1:b2,c1:c2]=result[0][:,:][a1-j]
+                    dist[b1:b2,c1:c2]=result[1][:,:][a1-j]
             else:
                 outf[:,i,:,a1:a2]=result[0]
                 dist[:,i,:,a1:a2]=result[1]
