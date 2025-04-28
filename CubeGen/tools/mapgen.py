523f35d14f8aa9c7eee6acca8262fd97fedfdba4
hjibarram <hjibarram@gmail.com>
2025-04-28 17:07:50 -0600

    Update mapgen.py

--- CubeGen/tools/mapgen.py
+++ CubeGen/tools/mapgen.py
@@ -59,7 +59,7 @@ def gen_map(expnumL,nameF='MapLVM',notebook=True,use_slitmap=True,cent=False,coo
         #if i == 0:
         #    equinox=hdr1['EQUINOX']
         #    print(equinox)
-        equinox=TIME(2024.8, format='jyear')
+        equinox=Time(2024.8, format='jyear')
         coord = SkyCoord(ra=ra_fib/3600.0, dec=dec_fib/3600.0, frame='fk5', equinox=equinox, unit='deg')
         newcoord = coord.transform_to(SkyCoord(ra=ra_fib/3600.0, dec=dec_fib/3600.0, frame='fk5', equinox='J2000').frame)
         new_ra_fib=newcoord.ra.deg
