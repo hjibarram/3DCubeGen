01049e9cbe1da6f68d42bf76d8723fdf64d0c935
hjibarram <hjibarram@gmail.com>
2025-05-01 00:10:01 -0600

    test

--- setup.py
+++ setup.py
@@ -44,8 +44,8 @@ version = version(VERSION_FILE)
 
 setup(
     name='3DCubeGen',
-    version=version,
-    #use_scm_version={"version_file": "CubeGen/common/_version.py"},
+    #version=version,
+    use_scm_version={"version_file": "CubeGen/common/_version.py"},
     description='A Python implementation for a 3D RSS to cube reconstruction',
     #long_description=readme(),
     classifiers=[
