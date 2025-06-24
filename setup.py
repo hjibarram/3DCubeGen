983fd77bf7a53c2c0c1758b7775586d6984f7f32
hjibarram <hjibarram@gmail.com>
2025-06-23 21:19:45 -0600

    updates

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ requirements = [
 
 #DATA_DIRNAME = 'legacy'
 #DATA_DIRS = ['legacy', 'megaratools/auxiliary']#megaradrp-calibrations-2018.1
-DATA_DIRS = ['legacy','megaratools/auxiliary','megaratools/auxiliary/megaradrp-calibrations-2018.1',
+DATA_DIRS = ['legacy','megaratools/auxiliary','megaratools/auxiliary/megaradrp-calibrations-20181',
 'megaratools/auxiliary/esostandars']
 SCRIPTS_DIRNAME = 'bin'
 VERSION_FILE = 'CubeGen/common/constants.py'
