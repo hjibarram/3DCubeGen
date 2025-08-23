b591ec8f90811ec6830e8bdc348a3e75a7e74850
hjibarram <hjibarram@gmail.com>
2025-08-23 16:44:07 -0600

    Update megkernel.py

--- CubeGen/megaratools/megkernel.py
+++ CubeGen/megaratools/megkernel.py
@@ -3,11 +3,10 @@ import numpy as np
 
 #spec_ifu,specE_ifu,x_ifu_V,y_ifu_V,fibA,pix_s,sigm_s,alph_s,yo,xi,xf,nw,nl,npros,nproc,erroF
 def kernel_int(ifu,ifuE,spec_ifu,x_ifu_V,y_ifu_V,fibA,pix_s,sigm_s,alph_s,yi,yf,xi,xf,nw,nl,i,erroF=False):
-    if sigm_s > fibA*3.5*2:
-        radiT=sigm_s*4#/2.0
+    if sigm_s > fibA*3.5:#*2:
+        radiT=sigm_s*4.0#/2.0
     else:
         radiT=fibA*3.5/2.0
-    print("Kernel radius: ", radiT, sigm_s, fibA)
     for j in range(0, nl):
         yi=yf
         yf=yf+pix_s
