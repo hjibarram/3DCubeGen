02a52bad5969e7add83ebfdf64b7b358cd1b3ec8
hjibarram <hjibarram@gmail.com>
2025-11-11 12:55:19 -0600

    Update mapgen.py

--- CubeGen/tools/mapgen.py
+++ CubeGen/tools/mapgen.py
@@ -155,7 +155,7 @@ def gen_map(expnumL,nameF='MapLVM',notebook=True,use_slitmap=True,cent=False,coo
                 x_ifu_pix[nfib0*i:nfib0*(i+1)]=x_pixel
                 y_ifu_pix[nfib0*i:nfib0*(i+1)]=y_pixel
                 #if np.nanmax(x_pixel) > 150:
-                print(np.nanmean(ra_fib/3600.0),np.nanmean(dec_fib/3600.0),file,'optionB')        
+                print(np.nanmin(np.abs(ra_fib)/3600.0),np.nanmin(np.abs(dec_fib)/3600.0),file,'optionB')        
         if pbars:
             pbar.update(1)     
     if pbars:
