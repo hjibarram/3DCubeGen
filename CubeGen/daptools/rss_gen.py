67fcc56c3ba39432a6876ed5ef0fa421e629f6fa
hjibarram <hjibarram@gmail.com>
2026-01-14 12:20:51 +0100

    Import interp1d from scipy.interpolate in rss_gen.py
    Added the interp1d import from scipy.interpolate to support interpolation functionality in the rss_gen.py module.

--- CubeGen/daptools/rss_gen.py
+++ CubeGen/daptools/rss_gen.py
@@ -6,6 +6,7 @@ import numpy as np
 import CubeGen.tools.tools as tools
 from astropy.wcs.utils import pixel_to_skycoord
 import warnings
+from scipy.interpolate import interp1d
 warnings.filterwarnings("ignore")
 
 def rssp_extract(name,path='./',path_out='./',basename_in='lvmCube-NAME.fits.gz',basename_out='lvmRSS-NAMElab.fits',pbars=True,flu16=True,nsplit=0,spt=[0,0],lvm=True,fluxu=1e-16,notebook=True):
