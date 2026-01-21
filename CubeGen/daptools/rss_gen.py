6a5d9adc129681a7d4d8c28b87fd00dc63ea9d10
hjibarram <hjibarram@gmail.com>
2026-01-21 13:01:46 +0100

    Update rss_gen.py

--- CubeGen/daptools/rss_gen.py
+++ CubeGen/daptools/rss_gen.py
@@ -683,8 +683,12 @@ def rssp_multi1d(namelist,path='./',path_in='./',path_out='./',hid='RA',sep=',',
     col19 = fits.Column(name='fibstatus', format='K', array=fibstatus)
     col20 = fits.Column(name='ra', format='D', array=ra)
     col21 = fits.Column(name='dec', format='D', array=dec)
+    fits_new_cols=[]
+    if len(keys_ext) > 0:
+        for i in range(len(keys_ext)):
+            fits_new_cols.extend([fits.Column(name=new_keys_ext[i], format=tools.numpy_to_tform(new_values[i]), array=new_values[i])])
     coldefs = fits.ColDefs([col1, col2, col3, col4, col5, col6, col7, col8, col9, 
-                            col10, col11, col12, col13, col14, col15, col16, col17, col18, col19, col20, col21])
+                            col10, col11, col12, col13, col14, col15, col16, col17, col18, col19, col20, col21, *fits_new_cols])
     h11 = fits.BinTableHDU.from_columns(coldefs)
     h_y=h11.header
     h_y['EXTNAME']='SLITMAP'
