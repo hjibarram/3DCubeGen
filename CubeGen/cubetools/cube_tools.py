860f9f63fb76b85f725f725166374a030f5e3f35
hjibarram <hjibarram@gmail.com>
2026-09-15 09:40:52 -0600

    Update cube_tools.py

--- CubeGen/cubetools/cube_tools.py
+++ CubeGen/cubetools/cube_tools.py
@@ -469,7 +469,7 @@ def crop_image(names, cube, dir1='./', dir2='./', dir3='./', apt='_gri'):
 
 
 def coad_cube(name, dir1='', dir2='', vphs=None, patch=True):
-     """
+    """
     Co-add reconstructed IFU datacubes from multiple spectral bands into
     a single wavelength-continuous datacube.
 
